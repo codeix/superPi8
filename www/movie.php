ca60cc3b86f9c096f11c9761dd8a096f03a88bd6
Samuel <samuel.riolo@swissonline.ch>
2014-11-30 02:09:38 +0100

    add server part

---
+++ www/movie.php
@@ -0,0 +1,139 @@
+<?php
+
+$DIR=realpath('../movies/movies');
+$MAP=array('webm' => array('suffix' => 'webm', 'mime' => 'Content-type: video/webm'),
+           'mp4' => array('suffix' => 'web.mp4', 'mime' => 'Content-type: video/mp4'));
+
+$type = $_REQUEST['type'];
+$path= $DIR.'/'.base64_decode($_REQUEST['movie']).'/'.base64_decode($_REQUEST['movie']).'.'.$MAP[$type]['suffix'];
+
+if (!file_exists($path)){
+    echo "kaput...";
+    die();
+}
+
+
+header($MAP[$type]['mime']);
+header("Content-Length: " . filesize($path));
+
+if ($_REQUEST['download']){
+
+    header("Content-Disposition: attachment; filename=" . basename($path));    
+    header("Content-Type: application/force-download");
+    header("Content-Type: application/octet-stream");
+    header("Content-Type: application/download");
+    header("Content-Description: File Transfer");             
+} else {
+    if (isset($_SERVER['HTTP_RANGE']))
+        header('Content-Type: ' . $MAP[$type]['mime']);
+}
+
+
+$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR] File:$path" . ($_REQUEST['download']?' as download':' as stream');
+file_put_contents('../logs/visitors.log', $line . PHP_EOL, FILE_APPEND);
+    
+if (isset($_SERVER['HTTP_RANGE']))
+   rangeDownload($path);
+else
+   readfile($path);
+
+
+
+// http://mobiforge.com/design-development/content-delivery-mobile-devices
+function rangeDownload($file) {
+ 
+	$fp = @fopen($file, 'rb');
+ 
+	$size   = filesize($file); // File size
+	$length = $size;           // Content length
+	$start  = 0;               // Start byte
+	$end    = $size - 1;       // End byte
+	// Now that we've gotten so far without errors we send the accept range header
+	/* At the moment we only support single ranges.
+	 * Multiple ranges requires some more work to ensure it works correctly
+	 * and comply with the spesifications: http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+	 *
+	 * Multirange support annouces itself with:
+	 * header('Accept-Ranges: bytes');
+	 *
+	 * Multirange content must be sent with multipart/byteranges mediatype,
+	 * (mediatype = mimetype)
+	 * as well as a boundry header to indicate the various chunks of data.
+	 */
+	header("Accept-Ranges: 0-$length");
+	// header('Accept-Ranges: bytes');
+	// multipart/byteranges
+	// http://www.w3.org/Protocols/rfc2616/rfc2616-sec19.html#sec19.2
+	if (isset($_SERVER['HTTP_RANGE'])) {
+ 
+		$c_start = $start;
+		$c_end   = $end;
+		// Extract the range string
+		list(, $range) = explode('=', $_SERVER['HTTP_RANGE'], 2);
+		// Make sure the client hasn't sent us a multibyte range
+		if (strpos($range, ',') !== false) {
+ 
+			// (?) Shoud this be issued here, or should the first
+			// range be used? Or should the header be ignored and
+			// we output the whole content?
+			header('HTTP/1.1 416 Requested Range Not Satisfiable');
+			header("Content-Range: bytes $start-$end/$size");
+			// (?) Echo some info to the client?
+			exit;
+		}
+		// If the range starts with an '-' we start from the beginning
+		// If not, we forward the file pointer
+		// And make sure to get the end byte if spesified
+		if ($range0 == '-') {
+ 
+			// The n-number of the last bytes is requested
+			$c_start = $size - substr($range, 1);
+		}
+		else {
+ 
+			$range  = explode('-', $range);
+			$c_start = $range[0];
+			$c_end   = (isset($range[1]) && is_numeric($range[1])) ? $range[1] : $size;
+		}
+		/* Check the range and make sure it's treated according to the specs.
+		 * http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+		 */
+		// End bytes can not be larger than $end.
+		$c_end = ($c_end > $end) ? $end : $c_end;
+		// Validate the requested range and return an error if it's not correct.
+		if ($c_start > $c_end || $c_start > $size - 1 || $c_end >= $size) {
+ 
+			header('HTTP/1.1 416 Requested Range Not Satisfiable');
+			header("Content-Range: bytes $start-$end/$size");
+			// (?) Echo some info to the client?
+			exit;
+		}
+		$start  = $c_start;
+		$end    = $c_end;
+		$length = $end - $start + 1; // Calculate new content length
+		fseek($fp, $start);
+		header('HTTP/1.1 206 Partial Content');
+	}
+	// Notify the client the byte range we'll be outputting
+	header("Content-Range: bytes $start-$end/$size");
+	header("Content-Length: $length");
+ 
+	// Start buffered download
+	$buffer = 1024 * 8;
+	while(!feof($fp) && ($p = ftell($fp)) <= $end) {
+ 
+		if ($p + $buffer > $end) {
+ 
+			// In case we're only outputtin a chunk, make sure we don't
+			// read past the length
+			$buffer = $end - $p + 1;
+		}
+		set_time_limit(0); // Reset time limit for big files
+		echo fread($fp, $buffer);
+		flush(); // Free up memory. Otherwise large files will trigger PHP's memory limit.
+	}
+ 
+	fclose($fp);
+ 
+}
+?>
